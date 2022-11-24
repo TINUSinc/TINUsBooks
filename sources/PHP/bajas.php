b0714d27b616e5f00caf4a969276d0ca29a8e693
Francisco Mendez <al289539@edu.uaa.mx>
2022-11-23 21:58:16 -0600

    Co-authored-by: GERARDO FEMAT DELGADO <al244371@edu.uaa.mx> Co-authored-by: Emmanuel Munoz <emunozc@users.noreply.github.com> armado de dev para echar a jalar este pedo

---
+++ sources/PHP/bajas.php
@@ -0,0 +1,9 @@
+<?php
+    include_once("conexion.php");
+
+    //Funciones a las que se mandan parametros de las bajas.
+
+    function borrarUsuario() {
+        
+    }
+?>
---
+++ sources/PHP/usuarios/manejoUsuarios.php
@@ -0,0 +1,17 @@
+<?php
+    include_once("../conexion.php");
+    function  getUsuario($username, $contrasena){
+        $query = "SELECT * FROM usuario;";
+        $resultado = $conexion -> query($query);
+        while ($fila = $resultado -> fetch_assoc()){
+            if($fila["Cuenta_usr"]==$username){
+                if($fila["Contrasena_usr"]==$contrasena){
+                    $usr=$fila["Cuenta_usr"];
+                    break;
+                }
+            }
+        }
+        return $usr;
+    }
+
+?>
