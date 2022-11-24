269a659207575b5fb158bf49e587db325183e651
Gerardo Femat Delgado <al244371@edu.uaa.mx>
2022-11-24 08:01:48 -0600

    Cambios a la conexion

--- sources/PHP/conexion.php
+++ sources/PHP/conexion.php
@@ -1,26 +1,25 @@
-<?php 
-    include_once("altas.php");
-    include_once("bajas.php");
-    include_once("consultas.php");
-    $servidor='localhost:33065';
-    $cuenta='root';
-    $password='';
-    $bd='tinusbooks';
-    $conexion = new mysqli($servidor,$cuenta,$password,$bd);
-    if($conexion->connect_errno):
-        
-?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        </head>
-        <body>
-            <div class="alert alert-danger" role="alert">
-            Error al conctar con la base de datos: <?php echo $conexion->connect_error?>
-            </div>
-        </body>
-        </html>
-<?endif?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php 
+        include_once("altas.php");
+        include_once("bajas.php");
+        include_once("consultas.php");
+        $servidor='localhost:33065';
+        $cuenta='root';
+        $password='';
+        $bd='tinusbooks';
+        $conexion = new mysqli($servidor,$cuenta,$password,$bd);
+        if($conexion->connect_errno): 
+    ?>
+    <div class="alert alert-danger" role="alert">
+    Error al conctar con la base de datos: <?php echo $conexion->connect_error?>
+    </div>
+    <?endif?>
+</body>
+</html>
