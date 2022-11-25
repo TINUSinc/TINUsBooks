27a91af7f1a563d9f356f534bfd88c210f90df3b
sandy02star <al292930@edu.uaa.mx>
2022-11-25 10:00:16 -0600

    hola, esto es para ver si jal<

---
+++ sources/PHP/formularios/categoria.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Categoria</title>
+        <meta name="viewport" content="width=device-width">
+
+        <!-- links de Bootstrap -->
+        <!-- CSS only -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        
+        <div class="text-center mt-5">
+            <h1 class="h2 mb-3 font-weight-normal">Registro de categorias</h1>
+            <form style="max-width:300px;margin:auto;">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Nombre de la categoria">
+                    <input type="text" class="mt-2 form-control" placeholder="Tipo de categoria">
+                    <div class="mt-3 d-grid gap-2">
+                        <button class="btn btn-dark btn-lg">Enviar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </body>
+
+</html>
