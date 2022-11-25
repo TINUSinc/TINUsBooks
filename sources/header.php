03a2685783fa49653be688a528178d3a6d770565
Gerardo Femat Delgado <al244371@edu.uaa.mx>
2022-11-25 17:36:25 -0600

    no se que hice

--- sources/header.php
+++ sources/header.php
@@ -111,158 +111,160 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-md navbar-dark text-black">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-                
-              </a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active text-white" aria-current="page" href="/">Inicio</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Certificaciones</a> 
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Contacto</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Acerda De</a>
-              </li>
-            </ul>
-            <div class="btn-group">
-              <button type="button" class="btn btn-secondary <?php if(isset($_SESSION["usuario"])){echo "disabled";}?>" 
-                      data-bs-toggle="modal" data-bs-target="#modalIniciar">
-                <?php 
-                  if(!isset($_SESSION["usuario"])){
-                    echo "Inciar sesión/Registrarse";
-                  }else{
-                    $saludo = "Bienvenido";
-                    $nom = $_SESSION['usuario']['Nombre_Usr']; echo "$saludo, $nom";
-                  }?>
-              </button>
-              <div class="modal fade" id="modalIniciar" tabindex="-1" aria-labelledby="modalIniciar" aria-hidden="true">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5 text-black" id="modalIniciar">Incia sesión</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                      <div class="modal-body">
-                        <div class="container-fluid">
-                          <div class="row align-items-center mx-4 my-4">
-                            <div class="col-3">
-                              <label for="usuario" class="col-form-label text-black">Usuario:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="text" id="usuario" class="form-control" name="usuario" value="<?php if(!empty($_COOKIE['usuario']))echo $_COOKIE['usuario'];?>" required>
-                            </div>
-                          </div>
-                          <div class="row align-items-center mx-4 my-4">
-                            <div class="col-3">
-                              <label for="contra" class="col-form-label text-black">Contraseña:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="password" id="contra" class="form-control" name="contra" required>
-                            </div>
-                          </div>
-                          <div class="input-group mb-3">
-                              <div class="input-group-text">
-                                <input class="form-check-input mt-0" type="checkbox" name="cookieUSR" id="cookieUSR" <?php if(!empty($_COOKIE['usuario'])){ echo 'checked';}?>>
-                              </div>
-                              <label for="cookie" class="form-control">¿Desea guardar su usuario para despues?</label>
-                            </div>
-                        </div>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-target="#modalRegistro" data-bs-toggle="modal">Registrarse</button>
-                        <button type="submit" class="btn btn-primary" name="sesion" >Inciar sesión</button>
-                      </div>
-                    </form>
+      <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+              
+            </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active text-white" aria-current="page" href="/">Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Certificaciones</a> 
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Contacto</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Acerda De</a>
+            </li>
+          </ul>
+          <div class="btn-group">
+            <button type="button" class="btn btn-secondary <?php if(isset($_SESSION["usuario"])){echo "disabled";}?>" 
+                    data-bs-toggle="modal" data-bs-target="#modalIniciar">
+              <?php 
+                if(!isset($_SESSION["usuario"])){
+                  echo "Inciar sesión/Registrarse";
+                }else{
+                  $saludo = "Bienvenido";
+                  $nom = $_SESSION['usuario']['Nombre_Usr']; 
+                  echo "$saludo, $nom";
+                }
+              ?>
+            </button>
+            <div class="modal fade" id="modalIniciar" tabindex="-1" aria-labelledby="modalIniciar" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5 text-black" id="modalIniciar">Incia sesión</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
-                </div>
-              </div>
-              <div class="modal fade" id="modalRegistro" aria-hidden="true" aria-labelledby="modalRegistro" tabindex="-1">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h1 class="modal-title fs-5 text-black" id="modalRegistro">Registro</h1>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <form autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-                      <div class="modal-body">
-                        <div class="container-fluid">
-                          <div class="row align-items-center mx-1 my-4">
-                            <div class="col-3">
-                              <label for="usuario" class="col-form-label text-black">Crea un usuario:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="text" id="usuario" class="form-control" name="usuario" required>
-                            </div>
+                  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                    <div class="modal-body">
+                      <div class="container-fluid">
+                        <div class="row align-items-center mx-4 my-4">
+                          <div class="col-3">
+                            <label for="usuario" class="col-form-label text-black">Usuario:</label>
                           </div>
-                          <div class="row align-items-center mx-1 my-4">
-                            <div class="col-3">
-                              <label for="contra" class="col-form-label text-black">Crea una contraseña:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="password" id="contra" class="form-control" name="contra" required>
-                            </div>
-                          </div>
-                          <div class="row align-items-center mx-1 my-4">
-                            <div class="col-3">
-                              <label for="contra2" class="col-form-label text-black">Confirme la contraseña:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="password" id="contra" class="form-control" name="contra2" required>
-                            </div>
-                          </div>
-                          <div class="row align-items-center mx-1 my-4">
-                            <div class="col-3">
-                              <label for="nombre" class="col-form-label text-black">Nombre completo:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="text" id="nombre" class="form-control" name="nombre" required>
-                            </div>
-                          </div>
-                          <div class="row align-items-center mx-1 my-4">
-                            <div class="col-3">
-                              <label for="correo" class="col-form-label text-black">Correo electrónico:</label>
-                            </div>
-                            <div class="col-9">
-                              <input type="email" id="correo" class="form-control" name="correo" required>
-                            </div>
+                          <div class="col-9">
+                            <input type="text" id="usuario" class="form-control" name="usuario" value="<?php if(!empty($_COOKIE['usuario']))echo $_COOKIE['usuario'];?>" required>
                           </div>
                         </div>
+                        <div class="row align-items-center mx-4 my-4">
+                          <div class="col-3">
+                            <label for="contra" class="col-form-label text-black">Contraseña:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="password" id="contra" class="form-control" name="contra" required>
+                          </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <div class="input-group-text">
+                              <input class="form-check-input mt-0" type="checkbox" name="cookieUSR" id="cookieUSR" <?php if(!empty($_COOKIE['usuario'])){ echo 'checked';}?>>
+                            </div>
+                            <label for="cookie" class="form-control">¿Desea guardar su usuario para despues?</label>
+                          </div>
                       </div>
-                      <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary" name="registro">Registrarse</button>
-                      </div>
-                    </form>
-                  </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-target="#modalRegistro" data-bs-toggle="modal">Registrarse</button>
+                      <button type="submit" class="btn btn-primary" name="sesion" >Inciar sesión</button>
+                    </div>
+                  </form>
                 </div>
               </div>
-              <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-              <span class="visually-hidden">Toggle Dropdown</span>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-lg-end dropdown-menu-sm-start ">
-                <li><a class="dropdown-item <?php if(isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/pagUsuarios.php">Perfil</a></li>
-                <?php 
-                  if(!empty($_SESSION) && isset($_SESSION['usuario'])):
-                ?>
-                <?php if($_SESSION['usuario']['Admin'] == 1): ?>
-                <li><a class="dropdown-item <?php if(isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/pagAdminExamenes.php">Examenes Usuarios</a></li>
-                <?php endif ?>
-                <?php endif ?>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item <?php if(isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/php/usuarios/cerrarSesion.php">Cerrar sesión</a></li>
-              </ul>
             </div>
+            <div class="modal fade" id="modalRegistro" aria-hidden="true" aria-labelledby="modalRegistro" tabindex="-1">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5 text-black" id="modalRegistro">Registro</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <form autocomplete="off" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                    <div class="modal-body">
+                      <div class="container-fluid">
+                        <div class="row align-items-center mx-1 my-4">
+                          <div class="col-3">
+                            <label for="usuario" class="col-form-label text-black">Crea un usuario:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="text" id="usuario" class="form-control" name="usuario" required>
+                          </div>
+                        </div>
+                        <div class="row align-items-center mx-1 my-4">
+                          <div class="col-3">
+                            <label for="contra" class="col-form-label text-black">Crea una contraseña:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="password" id="contra" class="form-control" name="contra" required>
+                          </div>
+                        </div>
+                        <div class="row align-items-center mx-1 my-4">
+                          <div class="col-3">
+                            <label for="contra2" class="col-form-label text-black">Confirme la contraseña:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="password" id="contra" class="form-control" name="contra2" required>
+                          </div>
+                        </div>
+                        <div class="row align-items-center mx-1 my-4">
+                          <div class="col-3">
+                            <label for="nombre" class="col-form-label text-black">Nombre completo:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="text" id="nombre" class="form-control" name="nombre" required>
+                          </div>
+                        </div>
+                        <div class="row align-items-center mx-1 my-4">
+                          <div class="col-3">
+                            <label for="correo" class="col-form-label text-black">Correo electrónico:</label>
+                          </div>
+                          <div class="col-9">
+                            <input type="email" id="correo" class="form-control" name="correo" required>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="submit" class="btn btn-primary" name="registro">Registrarse</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
+              <span class="visually-hidden">Toggle Dropdown</span>
+            </button>
+            <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-lg-end dropdown-menu-sm-start">
+              <li><a class="dropdown-item" <?php if(!isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/pagUsuarios.php">Perfil</a></li>
+              <?php 
+                if(!empty($_SESSION) && isset($_SESSION['usuario'])):
+              ?>
+              <?php if($_SESSION['usuario']['Admin'] == 1): ?>
+              <li><a class="dropdown-item" <?php if(!isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/pagAdminExamenes.php">Examenes Usuarios</a></li>
+              <?php endif ?>
+              <?php endif ?>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" <?php if(!isset($_SESSION["usuario"])){echo "disabled";}?>" href="/sources/php/usuarios/cerrarSesion.php">Cerrar sesión</a></li>
+            </ul>
           </div>
         </div>
-      </nav>
+      </div>
+    </nav>
 </body>
 </html>
