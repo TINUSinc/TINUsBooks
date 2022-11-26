20da60870e48f872712cdb47ae2fa32bef122054
ffabioladel <al243453@edu.uaa.mx>
2022-11-26 00:54:22 -0600

    Cards con productos a agregar

---
+++ sources/PHP/productos.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Productos</title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="style.css">
+
+<style>
+    body {
+  background: #232321;
+}
+
+h1 {
+  color: #fff;
+  padding: 10px 0;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  border: none;
+}
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
+}
+.card a {
+  color: initial;
+}
+.card a:hover {
+  text-decoration: initial;
+}
+.card .text-muted i {
+  margin: 0 10px;
+}
+</style>
+
+</head>
+<body>
+<div class="container">
+  <div class="text-center">
+    <h1>Productos</h1>
+  </div>
+  <div class="container">
+    <div class="card-columns">
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="Aqui van las imagenes">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text">bla bla </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+      </div>
+        </a>
+      </div>
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title">  Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto  </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto Descripcion del producto </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      
+      <div class="card">
+        <a href="#">
+        <img class="card-img-top" src="shrek.jpeg" alt="image">
+        <div class="card-body">
+          <h5 class="card-title"> Nombre del producto </h5>
+          <p class="card-text"> Descripcion del producto  </p>
+          <p class="card-text"><small class="text-muted"><i class="fas fa-shopping-cart"></i>Agregar al carrito</small></p>
+        </div>
+        </a>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+  
+</body>
+</html>
