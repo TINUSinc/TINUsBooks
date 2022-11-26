e860e9367b2205adcbbb1de9895c0a341de8d524
Gerardo Femat Delgado <al244371@edu.uaa.mx>
2022-11-26 16:56:04 -0600

    Hay una llista con las imagenes disponibles de los productos

--- js/productos.js
+++ js/productos.js
@@ -6,13 +6,20 @@ var existencias = document.getElementById("existencias");
 var descuento = document.getElementById("descuento");
 var categoria = document.querySelector('#categoria');
 var carrousel = document.getElementById("carrousel");
+var botonEnviar = document.getElementById("btnEnviar");
+var botonElim = document.getElementById("btnEliminar");
+var selectImagenes = document.getElementById("imagenesSubidas");
+var nuevoNombre = document.getElementById("nuevoNombreImagen");
+
+
 producto.addEventListener("change", function(){
     var seleccion = producto.options[producto.selectedIndex].value;
     if(seleccion != 0){
         $.post("../PHP/consultas.php",{"idProd":seleccion},"json")
         .done(function(data,textstatus,jqXHR){
             var datos = JSON.parse(data);
-            console.log(datos);
+            botonElim.removeAttribute("disabled");
+            botonEnviar.innerHTML = "Modificar";
             nombre.value = datos.Nombre_Prod;
             precio.value = datos.Precio_Prod;
             descripcion.value = datos.Descripcion_Prod;
@@ -24,22 +31,34 @@ producto.addEventListener("change", function(){
                 var datos = JSON.parse(data);
                 datos = Object.values(datos);
                 carrousel.innerHTML = "";
-                datos.forEach(function(currentValue, index) {
-                    var agregar;
-                    agregar = "<div class='carousel-item'><img src='/media/productos/"+currentValue+"' class='d-block w-100'></div>";
-                    if(index == 0) agregar = "<div class='carousel-item active'><img src='/media/productos/"+currentValue+"' class='d-block w-100'></div>"; 
-                    carrousel.innerHTML += agregar;
-                });
+                selectImagenes.innerHTML="";
                 if(datos.length === 0){
                     carrousel.innerHTML = "<div class='carousel-item active'><img src='/media/productos/No image.jpg' class='d-block w-100'></div>";
+                    selectImagenes.innerHTML="<option value='0' disabled selected>Sin Imagenes</option>";
+                    nuevoNombre.setAttribute("disabled","");
+                }else{
+                    datos.forEach(function(currentValue, index) {
+                        var agregarOpcion;
+                        var agregar;
+                        agregarOpcion = "<option value='"+currentValue+"'>"+currentValue+"</option>";
+                        agregar = "<div class='carousel-item'><img src='/media/productos/"+currentValue+"' class='d-block w-100'><div class='carousel-caption d-none d-md-block'><h5>"+currentValue+"</h5></div></div>";
+                        if(index == 0) agregar = "<div class='carousel-item active'><img src='/media/productos/"+currentValue+"' class='d-block w-100'><div class='carousel-caption d-none d-md-block'><h5>"+currentValue+"</h5></div></div>"; 
+                        carrousel.innerHTML += agregar;
+                        selectImagenes.innerHTML+=agregarOpcion;
+                        nuevoNombre.removeAttribute("disabled");
+                    });
                 }
             });
             
         })
         .fail(function(jqXHR, textStatus, errorThrown){
-            console.log("Solicitud fallada");
+            
         });
     }else{
+        nuevoNombre.setAttribute("disabled","");
+        botonElim.setAttribute("disabled","");
+        botonEnviar.innerHTML = "Crear";
+        selectImagenes.innerHTML="<option value='0' disabled selected>Sin Imagenes</option>";
         nombre.value = "";
         precio.value = "";
         descripcion.value = "";
--- sources/header.php
+++ sources/header.php
@@ -124,7 +124,7 @@
     
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     
-    <script src="js/captcha.js"></script>
+    <script src="/js/captcha.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js" defer></script>
 
     <link rel="stylesheet" href= "https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
