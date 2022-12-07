ae59965ac5df3707cae15a73d7ec0887bafb3c1f
Jacqueline-Renito <al291218@edu.uaa.mx>
2022-12-06 22:20:02 -0600

    mejorando graficas

--- sources/Administracion/graficas.php
+++ sources/Administracion/graficas.php
@@ -2,17 +2,26 @@
     include("adminNavBar.php");
 ?>
 
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../../css/graficasStyle.css">
+    <title>Gráficas</title>
 </head>
 
 <body>
-   
-   <form method="post" action="graficas.php">
-    <select name="mes">
-        <?php
+    <h3>Gráficas</h3>
+    <div class="contenedor">
+        <br>
+        <div class="grafica1">
+            <h2>Compras</h2>
+            <form method="post" action="graficas.php">
+                <select name="mes">
+                    <?php
         for ($i=1; $i<=12; $i++) {
             if ($i == date('m'))
                 echo '<option value="'.$i.'" selected>'.$i.'</option>';
@@ -20,10 +29,10 @@
                 echo '<option value="'.$i.'">'.$i.'</option>';
         }
         ?>
-    </select>
+                </select>
 
-    <select name="ano">
-        <?php
+                <select name="ano">
+                    <?php
         for($i=date('o'); $i>=2022; $i--){
             if ($i == date('o'))
                 echo '<option value="'.$i.'" selected>'.$i.'</option>';
@@ -32,39 +41,77 @@
         }
         ?>
 
-    </select>
-    
-    <p><input type="submit" name="enviar" value="enviar"></p>
-    
-    </form>
-    <?php
+                </select>
+                <br><br>
+                <p id="btnEnviar"><input type="submit" name="enviar" value="Enviar"></p>
+
+            </form>
+            <?php
         if (isset($_POST['enviar'])) {
+            switch ($_POST['mes']) {
+                case 1:
+                    $mes="Enero";
+                    break;
+                case 2:
+                    $mes="Febrero";
+                    break;
+                case 3:
+                    $mes="Marzo";
+                    break;
+                case 4:
+                    $mes="Abril";
+                    break;
+                case 5:
+                    $mes="Mayo";
+                    break;
+                case 6:
+                    $mes="Junio";
+                    break;
+                case 7:
+                    $mes="Julio";
+                    break;
+                case 8:
+                    $mes="Agosto";
+                    break;
+                case 9:
+                    $mes="Septiembre";
+                    break;
+                case 10:
+                    $mes="Octubre";
+                    break;
+                case 11:
+                    $mes="Noviembre";
+                    break;
+                case 12:
+                    $mes="Diciembre";
+                    break;
+            }
+            $anio = $_POST['ano'];
            // echo 'Fecha recibida: '.$_POST['dia'].'/'.$_POST['mes'].'/'.$_POST['ano'];?>
 
-    <script type="text/javascript">
-        // Load the Visualization API and the corechart package.
-        google.charts.load('current', {
-            'packages': ['corechart']
-        });
+            <script type="text/javascript">
+            // Load the Visualization API and the corechart package.
+            google.charts.load('current', {
+                'packages': ['corechart']
+            });
 
-        // Set a callback to run when the Google Visualization API is loaded.
-        google.charts.setOnLoadCallback(drawChart);
+            // Set a callback to run when the Google Visualization API is loaded.
+            google.charts.setOnLoadCallback(drawChart);
 
-        // Callback that creates and populates a data table,
-        // instantiates the pie chart, passes in the data and
-        // draws it.
+            // Callback that creates and populates a data table,
+            // instantiates the pie chart, passes in the data and
+            // draws it.
 
+            function drawChart() {
 
-        function drawChart() {
+                // Create the data table.
+                var data = new google.visualization.DataTable([]);
 
-            // Create the data table.
-            var data = new google.visualization.DataTable([]);
+                data.addColumn('string', 'Categoria');
+                data.addColumn('number', 'Cantidad');
 
-            data.addColumn('string', 'Categoria');
-            data.addColumn('number', 'Cantidad');
-
-            data.addRows([
-                <?php
+                data.addRows([
+                    <?php
                 $numConsulta = getVentasMes($_POST['mes'], $_POST['ano']);
                 
                 if ($numConsulta == 0) {
@@ -75,29 +122,36 @@
                     }
                     }
                     ?>
-            ]);
+                ]);
 
 
-        // Set chart options
-        var options = {
-            'title': 'Compras',
-            'width': 400,
-            'height': 300
-        };
+                // Set chart options
+                var options = {
+                    'title': <?php echo "'" .$mes." ".$anio. "'" ?>,
+                    'width': 550,
+                    'height': 600,
+                    'backgroundColor': {
+                        fill: "gainsboro"
+                    },
+                    'pieHole': 0.4
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-        }
+                };
 
-    </script>
-<?php
+                // Instantiate and draw our chart, passing in some options.
+                var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+                chart.draw(data, options);
+            }
+            </script>
+
+            <?php
     }
     ?>
 
 
-    <!--Div that will hold the pie chart-->
-    <div id="chart_div"></div>
+            <!--Div that will hold the pie chart-->
+            <div id="chart_div"></div>
+        </div>
+    </div>
 </body>
 
 </html>
--- sources/Paginas/productos.php
+++ sources/Paginas/productos.php
@@ -19,10 +19,30 @@
     <h1>Productos</h1>
   </div>
   <div class="container">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 justify-content-end text-center">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="GET">
+        <select class="form-select" name="categoria" id="categoria" tittle="Elige una categoria de la lista" required>
+          <option value="0">Todas las categorías</option>
+          <?php
+              $categorias = getCategorias();
+              foreach($categorias as $categoria){
+                  echo '<option value='.$categoria["ID_cat"].'>'.$categoria["Nom_Cat"].'</option>';
+              }
+          ?>
+        </select>
+        <input type="submit" class="btn btn-light my-2" value="Mostrar">
+      </form>
+    </div>
+    <br>
     <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 g-4">
-      <?php 
-        $productos=getProductos();
-        $band = false;               
+      <?php
+        if(isset($_GET["categoria"]) && !empty($_GET["categoria"])){
+          $productos = getProductosCategoria($_GET["categoria"]);
+        }else{
+          $productos=getProductos();
+        }
+        $band = false;
+      if(!empty($productos)):
         foreach($productos as $producto):
           if(!$band && $producto["Descuento_Prod"]==0 && rand(1,3) == 3){
             $cantidad = rand(55,80);
@@ -75,6 +95,11 @@
             </div>
           </div>
         <?php endforeach?>
+        <?php else: ?>
+          <div class='alert alert-warning show text-center' style="width: 100%;" role='alert'>
+              No hay productos para esta categoría.
+          </div>
+        <?php endif ?>
     </div>
   </div>
 </div>
