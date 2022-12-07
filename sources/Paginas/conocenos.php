379f440f37bfb9940f1903936cb984c44e625cb4
Jacqueline-Renito <al291218@edu.uaa.mx>
2022-12-06 19:38:29 -0600

    pagina conocenos

--- sources/Paginas/conocenos.php
+++ sources/Paginas/conocenos.php
@@ -14,7 +14,7 @@
         integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/7491ec4faf.js" crossorigin="anonymous"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/conocenos.css">
+    <link rel="stylesheet" href="../../css/conocenos.css">
 
 </head>
 
@@ -23,133 +23,140 @@
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
 
-    <div class="row">
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px; border-color: white;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
-                    </div>
-                </div>
+    <div class="containerTransparente">
+        <img src="../../media/nosotros/Historia.jpg" alt="Conócenos">
+        <div class="contentTransparente">
+            <div class="contenedorTexto">
+                <br><br>
+                <h1 id="titImagenTransparente">Nuestra Historia</h1>
+                <br>
+                <p id="txtImagenTransparente">Al fundar TINU's Books, teníamos un sólo objetivo en mente: crear una
+                    tienda online que ofreciera la más alta calidad, con las mejores opciones para realizar tus compras
+                    y con alto nivel de confiabilidad. Nuestra pasión por la excelencia nos condujo a materializar esta
+                    misión, siendo ésta la parte fundamental que nos ha impulsado a seguir adelante. Sabemos que cada
+                    producto es importante, por lo que procuramos que todo el proceso sea lo más agradable posible.
+                    ¡Echa un vistazo y compruébalo por tu cuenta!</p>
             </div>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px; border-color: white;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
+    <div class="contenedorCards">
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Fabiola.jpeg" alt="persona">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Fabiola Delgado</h5>
+                        <p class="card-text">Tengo 20 años, me gusta ir al gimnasio. Me considero una persona muy artística pues me gusta tocar el piano y pintar</p>
+                        <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/ffabiolad" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.facebook.com/ffabiolad" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
+
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Gerardo.png" alt="persona">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Gerardo Femat</h5>
+                        <p class="card-text">Soy una persona muy inteligente, me gusta mucho desafiarme en matemáticas y dar asesorías a mis compañeros que no les funciona el cerebro</p>
+                        <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/geraer.jui1" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.instagram.com/gefemadel/" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col">
-            <div class="card mb-3" style="max-width: 540px;">
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img class="imgCircular" src="/media/nosotros/persona.jpg" alt="persona">
-                    </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Nombre</h5>
-                            <p class="card-text">Descripcion</p>
-                            <p class="card-text"><small class="text-muted"><i
-                                        class="fa-brands fa-square-facebook fa-2x"></i><i style="padding-left:5px ;"
-                                        class="fa-brands fa-square-instagram fa-2x"></i></small></p>
-                            <hr>
-                            <p class="card-text">Descripcion</p>
-                        </div>
+
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Sandra.jpeg" alt="Sandra Prieto">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Sandra Olimpia</h5>
+                        <p class="card-text">Tengo 20 años, me gusta ver series, películas y animes, además de escuchar
+                            música. Me gustan mucho los animales, sobre todo los perritos y mi color favorito es el
+                            morado</p>
+                            <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/profile.php?id=100008825409891" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.instagram.com/sandyolimpia02/" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
                     </div>
                 </div>
             </div>
         </div>
+
+
     </div>
 
+    <div class="contenedorCards">
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Emmanuel.png" alt="persona">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Emmanuel Muñoz</h5>
+                        <p class="card-text">Soy un fifas, le voy al Amierdica pero acepto que el mejor equipo es el Necaxa, Fuerza Rayos. Yo hice el captcha y me quedo genial</p>
+                        <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/emunozce.6" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.instagram.com/mannymunoz_/" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Jacqueline.jpeg" alt="persona">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Jacqueline Renovato</h5>
+                        <p class="card-text">Tengo 19 años, mi color favorito es el rosa y mi banda favorita es Big Time Rush. Me gusta escuchar música y ver películas</p>
+                        <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/maricelajacqueline.renovatoramirez/" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.instagram.com/jacky.rrmz/" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mb-3" id="card">
+            <div class="row g-0">
+                <div class="col-md-4" id="imagen">
+                    <img class="imgCircular" src="../../media/nosotros/Francisco.jpeg" alt="persona">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Franciso Mendez</h5>
+                        <p class="card-text">Tengo 20 años, me gusta hacer streams y programar es uno de mis pasatiempos favoritos</p>
+                        <p class="card-text"><small class="text-muted"><a href="https://www.facebook.com/franciscodejesus.mendezlara" target="_blank"><i class="fa-brands fa-square-facebook fa-2x"
+                                    style="color: white"></i></a><a href="https://www.instagram.com/franciscomendez324/" target="_blank"><i style="padding-left:5px;color:white;"
+                                    class="fa-brands fa-square-instagram fa-2x" style="color: white"></i></a></small></p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 
 </body>
 
