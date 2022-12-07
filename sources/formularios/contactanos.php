fe7cce1542b90f5af2ccde27fed67077527fd225
ffabioladel <al243453@edu.uaa.mx>
2022-12-06 19:26:12 -0600

    Pagina de contactanos y mas cambios al index

---
+++ sources/formularios/contactanos.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contactanos</title>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="../../css/forms.css">
+    
+</head>
+
+<body>
+  <section class="formulario">
+   <div class="container contact">
+	<div class="row">
+		<div class="col-md-3">
+			<div class="contact-info">
+				<img src="https://image.ibb.co/kUASdV/contact-image.png" alt="image"/>
+				<h2>Contactános</h2>
+				<h4> Tu voz es muy importante </h4>
+			</div>
+		</div>
+		<div class="col-md-9">
+			<div class="contact-form">
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="fname">Nombre:</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="fname" placeholder="Ingresa tu nombre" name="fname">
+				  </div>
+				</div>
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="lname">Apellido:</label>
+				  <div class="col-sm-10">          
+					<input type="text" class="form-control" id="lname" placeholder="Ingresa tu apellido" name="lname">
+				  </div>
+				</div>
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="email">Email:</label>
+				  <div class="col-sm-10">
+					<input type="email" class="form-control" id="email" placeholder="Ingresa tu email" name="email">
+				  </div>
+				</div>
+				<div class="form-group">
+				  <label class="control-label col-sm-2" for="comment">Comentario:</label>
+				  <div class="col-sm-10">
+					<textarea class="form-control" rows="5" id="comment"></textarea>
+				  </div>
+				</div>
+				<div class="form-group">        
+				  <div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default">Enviar</button>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</section>
+
+<section class="informacion">
+    <h3>Conoce a nuestros distribuidores</h3>
+    <h3 style="font-weight: lighter">Encuentra una sucursal</h3>
+    <p>Distribuidor Aguascalientes Sur</p>
+    <p> Av. Las Americas #500</p>
+    <p> Aguascalientes, 1604</p><br><br>
+
+    <p>Distribuidor Rincón de Romos</p>
+    <p>Margaritas #122</p>
+    <p>Aguascalientes, 06140</p><br><br>
+
+    <p>Distribuidor Aguascalientes Norte</p>
+    <p> Av. Siglo XXI #400</p>
+    <p>Aguascalientes, 06140</p>
+</section>
+
+<!-- Barra descuento  --> 
+<section class="descuento">
+    <img class="zoom" src="../../media/indexmedia/linear.jpg" alt="">
+    
+    <h2>¡Obten 20% en toda la tienda!</h2>
+    <h5>Código: apertura20</h5>
+</section>
+
+<!-- Mapa --> 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3702.233231530003!2d-102.29467398539583!3d21.887092785539757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8429ee6733bab0cb%3A0x64f5f203073c9e2a!2sCalle%20Gral.%20Ignacio%20Zaragoza%2C%20Zona%20Centro%2C%20Aguascalientes%2C%20Ags.!5e0!3m2!1ses-419!2smx!4v1670296660311!5m2!1ses-419!2smx" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+<!-- Termina mapa -->
+
+</body>
+</html>
