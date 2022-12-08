f3b31ad44afcf126950c7b9e4058e1bc9b8a09ff
Gerardo Femat Delgado <al244371@edu.uaa.mx>
2022-12-07 19:40:35 -0600

    Responsive design de la página de conocenos

--- sources/Paginas/conocenos.php
+++ sources/Paginas/conocenos.php
@@ -11,15 +11,12 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../../css/conocenos.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
-    </script>
 
 </head>
 
 <body>
     <div class="containerTransparente">
-        <img src="../../media/nosotros/Historia.jpg" alt="Conócenos">
+        <img src="../../media/nosotros/Historia.jpg" id="imgConocenos" alt="Conócenos">
         <div class="contentTransparente">
             <div class="contenedorTexto">
                 <br><br>
@@ -34,10 +31,10 @@
             </div>
         </div>
     </div>
-
     <br><br>
     <h3>Conoce al team</h3>
     <div class="contenedorCards">
+    
     <br><br>
         <div class="card mb-3" id="card">
             <div class="row g-0">
@@ -108,11 +105,6 @@
                 </div>
             </div>
         </div>
-
-
-    </div>
-
-    <div class="contenedorCards">
         <div class="card mb-3" id="card">
             <div class="row g-0">
                 <div class="col-md-4" id="imagen">
