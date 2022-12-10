1de389a4bd3100d38394cded31a07deab657d602
Emmanuel Munoz <emunozce80@gmail.com>
2022-12-09 23:13:59 -0600

    Valid de name en formulario y contrasena (maomeno)

--- js/validarForms.js
+++ js/validarForms.js
@@ -1,4 +1,8 @@
 var char;
+var nombre = true;
+var lname = true;
+var contra1;
+var contra2;
 
 function containsNumber(str) {
     return /\d/.test(str); //Analiza si hay un caracter que tenga el valor de lo que esta dentro de los corchetes
@@ -12,6 +16,20 @@ function comprobarNameFormContac() {
         string = document.getElementById("fname").value; //Se obtiene la string completa
         if (containsNumber(string) == true) { //Se llama a la funcion y se checa si hay numeros en la cadena
             $("#textErrfname").show();
+            
+        }
+    });
+    $("#fname").keyup(function () {//Cuando se termina de presionar una tecla del keyboard...
+        string = document.getElementById("fname").value; //Se obtiene la string completa
+        if (containsNumber(string) == true) { //Se llama a la funcion y se checa si hay numeros en la cadena
+            nombre = false;
+            $("#textErrfname").show();
+            $("#boton-contacto").hide();
+        } else {
+            nombre = true;
+            if(nombre && lname) {
+                $("#boton-contacto").show();
+            }
         }
     });
 }
@@ -40,6 +58,19 @@ function comprobarlNameFormContact() {
             $("#textErrlname").show();
         }
     });
+    $("#lname").keyup(function () {//Cuando se termina de presionar una tecla del keyboard...
+        string = document.getElementById("lname").value; //Se obtiene la string completa
+        if (containsNumber(string) == true) { //Se llama a la funcion y se checa si hay numeros en la cadena
+            lname = false;
+            $("#textErrlname").show();
+            $("#boton-contacto").hide();
+        } else {
+            lname = true;
+            if(nombre && lname) {
+                $("#boton-contacto").show();
+            }
+        }
+    });
 }
 
 function validlNameFormContact() {
@@ -61,17 +92,29 @@ function validlNameFormContact() {
 
 //---------------------------------------------------Termina area de Contactanos-------------------------------------------------
 
+//---------------------------------------------------Empieza registro-------------------------------------------------
 
 function comprobarNameForm() {
     $("#nombre").focusout(function () {//Cuando se quita el focus del input...
         string = document.getElementById("nombre").value; //Se obtiene la string completa
         if (containsNumber(string) == true) { //Se llama a la funcion y se checa si hay numeros en la cadena
             $("#textErr").show();
+            $("#boton_registro").hide();
+        } else {
+            $("#boton_registro").show();
+        }
+    });
+    $("#nombre").keyup(function () {//Cuando se termina de presionar una tecla del keyboard...
+        string = document.getElementById("nombre").value; //Se obtiene la string completa
+        if (containsNumber(string) == true) { //Se llama a la funcion y se checa si hay numeros en la cadena
+            $("#textErr").show();
+            $("#boton_registro").hide();
+        } else {
+            $("#boton_registro").show();
         }
     });
 }
 
-//---------------------------------------------------Empieza registro-------------------------------------------------
 
 function validNameForm() {
     $(document).ready(function () { //Carga la funcion cuando document esta ready
@@ -80,13 +123,52 @@ function validNameForm() {
             const element = char[char.length - 1]; //Obtiene el ultimo caracter ingresado
             if (isNaN(element) || (element == ' ')) { //Si no es un numero... 
                 $("#textErr").hide(); //El mensaje de error se esconde
+                $("#boton_registro").show();
                 comprobarNameForm();
             } else { //Si resulta aparecer un numero en el ultimo caracter...
                 $("#textErr").show(); //El mensaje de error se muestra
+                $("#boton_registro").hide();
                 comprobarNameForm();
             }
         });
+    });
+}
 
+function validarContrasena() {
+    $(document).ready(function () {
+        $("#contraConfi").focusout(function () {//Cuando se quita el focus del input...
+            contra1 = document.getElementById("contra").value;
+            contra2 = document.getElementById("contraConfi").value;
+            if(contra1 == contra2) {
+                $("#textErrPS").hide();
+                $("#boton_registro").show();
+            } else {
+                $("#textErrPS").show();
+                $("#boton_registro").hide();
+            }
+        });
+        $("#contraConfi").keyup(function () {//Cuando se termina de presionar una tecla del keyboard...
+            contra1 = document.getElementById("contra").value;
+            contra2 = document.getElementById("contraConfi").value;
+            if(contra1 == contra2) {
+                $("#textErrPS").hide();
+                $("#boton_registro").show();
+            } else {
+                $("#textErrPS").show();
+                $("#boton_registro").hide();
+            }
+        });
+        $("#contra").keyup(function () {//Cuando se quita el focus del input...
+            contra1 = document.getElementById("contra").value;
+            contra2 = document.getElementById("contraConfi").value;
+            if(contra1 == contra2) {
+                $("#textErrPS").hide();
+                $("#boton_registro").show();
+            } else {
+                $("#textErrPS").show();
+                $("#boton_registro").hide();
+            }
+        });
     });
 }
 
